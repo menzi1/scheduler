af305c8575e2feebebc56620bf66dde2ef121da7
Menzi simelane <menzis@swade.co.sz>
2017-02-03 13:26:23 +0200

    my first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,27 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+  
+  def about
+  end
+  
+  def contact
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    else
+      flash.now[:error] = 'Cannot send message.'
+      render :new
+    end
+  end
+end
+end
